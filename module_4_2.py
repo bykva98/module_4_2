8823434033a389bdf1a9b5d0477a552914ae2af6
bykva98 <bykva.98@mail.ru>
2024-10-24 02:42:25 +0700

    Add files via upload

---
+++ module_4_2.py
@@ -0,0 +1,8 @@
+def test_function():
+    def inner_function():
+        print("Я в области видимости функции test_function")
+    inner_function()
+
+
+#inner_function() - ошибка: NameError: name 'inner_function' is not defined. Did you mean: 'test_function'?
+test_function()
